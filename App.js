76157c81b12ae63b6c8ccfe9b7012aace52826e9
Mohammed Ahmed ullah <ahmedsohelcu@gmail.com>
2019-10-14 15:06:21 +0600

    update simple branch

--- App.js
+++ App.js
@@ -74,6 +74,20 @@ export class LotsOfStyles extends React.Component {
                           </Text>
                     </View>
 
+                    <View style={styles.container_2}>
+                          <Text style={[styles.bigBlue,styles.mB10 ]}>PHP Laravel Framework শেখার আগে কি কি বিষয় জানতে হবে? </Text>
+                          <Text style={styles.f20}>
+                            PHP Laravel Framework শেখা শুরু করার আগে, আপনাকে অবশ্যই নিম্নের বিষয় গুলো জানতে হবে:
+                          </Text>
+                          <Text style={styles.bullet}>1. PHP</Text>
+                          <Text style={styles.bullet}>2. Object Oriented Concepts এবং</Text>
+                          <Text style={styles.bullet}>3. MVC structure সম্পর্কে পরিষ্কার ধারণা।</Text>
+
+                          <Text style={styles.bullet}>
+                            4.আর Laravel Framework এর Dependency Manage করার জন্য Composer অবশ্যই ইন্সটল করা থাকতে হবে।
+                          </Text>
+                    </View>
+
                     <View style={{width: 360, height: 90, backgroundColor: '#000'}}>
                         <Text style={{color:'#fff',marginTop:10,textAlign:'center'}}>This Application is Developed by :</Text>
                         <Text style={{color:'#fff',marginTop:10,textAlign:'center'}}>Ahmed sohel...</Text>
@@ -96,10 +110,14 @@ const styles = StyleSheet.create({
     padding:10,
     backgroundColor:'#ffebcd',
   },
+  container_2:{
+    padding:10,
+    backgroundColor:'#ffffff',
+  },
   bigBlue: {
     color: 'blue',
     fontWeight: 'bold',
-    fontSize: 30,
+    fontSize: 20,
   },
   red: {
     color: 'red',
